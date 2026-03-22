eba4af24feb7e9871dae8ae0fb84d568604be4f0
chmssou <rezazguichamsddin@gmail.com>
2026-03-22 21:16:09 +0100

    fix sanity config v4

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,10 +1,8 @@
 'use client'
 import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
 import {apiVersion, dataset, projectId} from './sanity/env'
 import {schema} from './sanity/schemaTypes'
-import {structure} from './sanity/structure'
 
 export default defineConfig({
   basePath: '/studio',
@@ -12,7 +10,6 @@ export default defineConfig({
   dataset,
   schema,
   plugins: [
-    structureTool({structure}),
     visionTool({defaultApiVersion: apiVersion}),
   ],
 })
